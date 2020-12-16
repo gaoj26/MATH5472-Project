75b2a78bfb65a2c634261c143524fb6dec57d7f0
Gao jing <43904188+gaoj26@users.noreply.github.com>
2020-12-16 09:30:37 +0800

    Delete dirrnd.m

--- dirrnd.m
+++
@@ -1,5 +0,0 @@
-function theta = dirrnd(alpha)
-% Dirichlet prior for word distribution
-% draw a sample from a dirichlet prior with the parameter vector alpha
-theta = randg(alpha);
-theta = theta/sum(theta);
